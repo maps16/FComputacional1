c777ed0a6795c948ae168ec4e91caebb6984e1c3
maps16 <mapsjr16@gmail.com>
2016-02-02 15:36:14 -0700

    Arreglo de formato

--- Actividad2/caida.py
+++ Actividad2/caida.py
@@ -1,4 +1,12 @@
+#Importar funcion sqtr
+from math import sqrt
+
+#Pedir al altura a usuario
 h = float(input("Proporciona la altura de la torre: "))
-t = float(input("Ingresa el tiempo: "))
-s = 0.5*9.81*t**2
-print("La altura de la pelota es", h-s,"metros")
+
+g=9.81 #Constate de la gravedad
+
+t=sqrt(2*h/g) #Calculo de tiempo de caida
+
+#Impresion de Resultados
+print'El tiempo de caida para una altura ',h,'m es: ', t 
