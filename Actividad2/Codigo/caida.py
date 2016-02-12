b57ecbe90b3a71dd69d717903c527e63ef514c38
maps16 <mapsjr16@gmail.com>
2016-02-12 13:22:30 -0700

    Se termino el reporte

---
+++ Actividad2/Codigo/caida.py
@@ -0,0 +1,12 @@
+#Importar funcion sqtr
+from math import sqrt
+
+#Pedir al altura a usuario
+h = float(input("Proporciona la altura en metros de la torre: "))
+
+g=9.81 #Constate de la gravedad
+
+t=sqrt(2*h/g) #Calculo de tiempo de caida
+
+#Impresion de Resultados
+print'El tiempo de caida para una altura ',h,'m es: ', t ,'s'
